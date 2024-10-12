c370ede892d8b912b3241194972d3dc6a58ff273
1DelFin1 <defario777@mail.ru>
2024-10-12 18:04:15 +0500

    изменен путь к function.js и поправлены функции

--- js/functions.js
+++ js/functions.js
@@ -1,30 +1,12 @@
 /* eslint-disable no-unused-vars */
-const checkIsLonger = (str, length) => {
-  if (str.length <= length) {
-    return true;
-  } else {
-    return false;
-  }
-};
+const checkIsLonger = (str, length) => str.length <= length;
 
 const checkIsPalindrom = (str) => {
   const helpStr = str.toLowerCase().replaceAll(' ', '');
-  let helpStr2 = '';
-  for (let i = helpStr.length - 1; i >= 0; i--) {
-    helpStr2 += helpStr[i];
-  }
-  if (helpStr === helpStr2) {
-    return true;
-  } else {
-    return false;
-  }
+  return helpStr === helpStr.split('').reverse().join('');
 };
 
 const checkIsInteger = (str) => {
   const result = str.toString().replace(/\D/g,'');
-  if (result.length > 0) {
-    return result;
-  } else {
-    return NaN;
-  }
+  return result.length > 0 ? result : NaN;
 };
