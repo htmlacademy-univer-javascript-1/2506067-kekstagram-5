27b850dcad60687bea43e728069c267362284f63
1DelFin1 <defario777@mail.ru>
2024-10-15 18:07:02 +0500

    main доделан

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
-function getRandomInt(min, max) {
+const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
 const names = [
   'Артём',
